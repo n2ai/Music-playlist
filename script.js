33a5ac00210599cea1926a6c8a9d012d87977b21
n2ai <longhai2511@gmail.com>
2022-11-03 14:39:27 -0500

    repeat button

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ musicAudio = wrapper.querySelector("#main-audio"),
 playPauseBtn = wrapper.querySelector(".play-pause"),
 prevBtn = wrapper.querySelector("#prev"),
 nextBtn = wrapper.querySelector("#next"),
-progressBar = wrapper.querySelector('.progress-bar')
+progressBar = wrapper.querySelector('.progress-bar'),
+progressArea = wrapper.querySelector('.progress-area')
 
 let musicIndex = 1;
 
@@ -67,16 +68,68 @@ nextBtn.addEventListener("click",nextMusic)
 
 prevBtn.addEventListener("click", prevMusic)
 
-progressBar.addEventListener("timeupdate", (e)=>{
+musicAudio.addEventListener("timeupdate", (e)=>{
     const currentTime = e.target.currentTime;
     const duration = e.target.duration;
     let progressWidth = (currentTime/duration)*100;
     progressBar.style.width =  `${progressWidth}%`;
+    let musicCurrentTime = wrapper.querySelector(".current-time")
+    let musicDuration = wrapper.querySelector(".max-duration")
 
-    musicAudio.addEventListener("loadeddata", ()=>{
-        let musicCurrentTime = wrapper.querySelector(".current-time")
-        let musicDuration = wrapper.querySelector(".max-duration")
+    musicAudio.addEventListener("loadeddata", ()=>{ 
+       
         
-        
+        // update total duration
+        let audioDuration = musicAudio.duration;
+        let totalMin = Math.floor(audioDuration/60)
+        let totalSec = Math.floor(audioDuration%60)
+        if (totalSec < 10){
+            totalSec = `0${totalSec}`
+        }
+        musicDuration.innerText = `${totalMin}:${totalSec}`
     })
-})  
+
+    //update current duration
+    let currentMin = Math.floor((currentTime)/60)
+    let currentSec = Math.floor((currentTime)%60)
+    if (currentSec < 10){
+        currentSec = `0${currentSec}`
+    }
+    musicCurrentTime.innerText = `${currentMin}:${currentSec}`
+})   
+
+// update song based on progress bar
+
+progressArea.addEventListener("click", (e)=>{
+    let progressWidthval = progressArea.clientWidth
+    let clickedOffSetX = e.offsetX
+    let songDuration = musicAudio.duration
+
+    musicAudio.currentTime = (clickedOffSetX/progressWidthval) * songDuration
+    playMusic()
+
+})
+
+//working on repeat button
+
+const repeatBtn = wrapper.querySelector('#repeat-plist')
+repeatBtn.addEventListener("click", ()=>{
+    let getText = repeatBtn.innerText
+
+    switch(getText){
+        case "repeat":
+            repeatBtn.innerText = "repeat_one"
+            repeatBtn.setAttribute("title", "Song Looped");
+            break;
+        case "repeat_one":
+            repeatBtn.innerText = "shuffle"
+            repeatBtn.setAttribute("title", "Playlist shuffle")
+            break;
+        case "shuffle":
+            repeatBtn.innerText = "repeat"
+            repeatBtn.setAttribute("title", "Playlist looped")
+            break;
+    }
+})
+
+//make repeat button work
